3a3b2b85fa3ccd4bb2d36024f26f1ff35d882a46
19ashutoshsingh <154892951+19ashutoshsingh@users.noreply.github.com>
2025-08-11 08:18:44 +0530

    created db connection and server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import connectDB from './config/db.js';
+import cors from 'cors';
+import 'dotenv/config';
+import authRoutes from './routes/authRoutes.js';
+import expenseRoutes from './routes/expenseRoutes.js';
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoutes);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
